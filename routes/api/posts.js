29f964afb62bb8e684d3a1fc7fe6abe17df7a068
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2020-12-31 19:56:58 +0530

    Like and unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -101,13 +101,13 @@ router.post('/createTextPost', auth, async (req, res) => {
 });
 
 //@route   POST /api/posts/getPostDetails
-//@desc    Create the Text post
+//@desc    Get post details
 //access   Private
 
 router.post('/getPostDetails', auth, async (req, res) => {
     const { postId } = req.body;
     try {
-        const post = await Post.findById(postId).populate({ path: 'creator', 'select': 'name profilePicture' });
+        const post = await Post.findById(postId).populate({ path: 'creator', 'select': 'name profilePicture username' });
         if (post) {
             const { isText, image, disableComments, caption, publishTime, likes, creator } = post;
             var responsePost = { isText, image, disableComments, caption, publishTime, creator, }
@@ -117,7 +117,80 @@ router.post('/getPostDetails', auth, async (req, res) => {
                 responsePost.isCreator = false;
             }
             responsePost.likes = likes.length;
+            responsePost.hasLiked = false;
+            const session = neodriver.session();
+            try {
+                const neo_res = await session.run(`return EXISTS((:User{ id : "${req.id}" })-[:LIKES]->(:Post{id : "${postId}"}))`);
+                responsePost.hasLiked = neo_res.records[0]._fields[0];
+            } catch (e) {
+                console.log(e);
+                await session.close()
+                return res.status(500).send("Unable to get post");
+            } then = async () => {
+                await session.close()
+            }
             return res.status(200).send(responsePost);
+        }
+        else {
+            return res.status(404).send("Post not found")
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("Server Error");
+    }
+});
+
+//@route   POST /api/posts/likePost
+//@desc    Like a post
+//access   Private
+
+router.post('/likePost', auth, async (req, res) => {
+    const { postId } = req.body;
+    try {
+        const post = await Post.findById(postId);
+        console.log(`MATCH (:User{ id : "${req.id}" }), (:Post{id : "${postId}"}) CREATE (u)-[:LIKES]->(p)`)
+        if (post) {
+            const session = neodriver.session();
+            try {
+                await session.run(`MATCH (u:User{ id : "${req.id}" }), (p:Post{id : "${postId}"}) CREATE (u)-[:LIKES]->(p)`);
+            } catch (e) {
+                console.log(e);
+                await session.close()
+                return res.status(500).send("Unable to like post");
+            } then = async () => {
+                await session.close()
+            }
+            return res.status(200).send("Done");
+        }
+        else {
+            return res.status(404).send("Post not found")
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("Server Error");
+    }
+});
+
+//@route   POST /api/posts/unlikePost
+//@desc    Like a post
+//access   Private
+
+router.post('/unlikePost', auth, async (req, res) => {
+    const { postId } = req.body;
+    try {
+        const post = await Post.findById(postId);
+        if (post) {
+            const session = neodriver.session();
+            try {
+                await session.run(`MATCH (:User{id : "${req.id}"})-[l:LIKES]->(:Post{id : "${postId}"}) DELETE l`);
+            } catch (e) {
+                console.log(e);
+                await session.close()
+                return res.status(500).send("Unable to like post");
+            } then = async () => {
+                await session.close()
+            }
+            return res.status(200).send("Done");
         }
         else {
             return res.status(404).send("Post not found")
