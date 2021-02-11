58f7257edcaef1874e69dc6ca8c35337b554a2b7
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2021-02-11 17:53:33 +0530

    Pagination for fetching feed posts

--- routes/api/feed.js
+++ routes/api/feed.js
@@ -13,15 +13,16 @@ const router = express.Router();
 //access   Private
 
 router.post('/getFeed', auth, async (req, res) => {
+    const { skip } = req.body;
     try {
         const session = neodriver.session();
         const b = new Date().toISOString();
-        console.log(`MATCH (:User{id:"${req.id}"})-[:IN_FEED]->(p:Post) WHERE p.expiryDate > "${b}" RETURN p.id`);
-        const neo_res = await session.run(`MATCH (:User{id:"${req.id}"})-[:IN_FEED]->(p:Post) WHERE p.expiryDate > "${b}" RETURN p.id ORDER BY p.publishDate DESC`);
+        console.log(`MATCH (:User{id:"${req.id}"})-[:IN_FEED]->(p:Post) WHERE p.expiryDate > "${b}" RETURN p.id SKIP ${skip} LIMIT 30`);
+        const neo_res = await session.run(`MATCH (:User{id:"${req.id}"})-[:IN_FEED]->(p:Post) WHERE p.expiryDate > "${b}" RETURN p.id ORDER BY p.publishDate DESC SKIP ${skip} LIMIT 30`);
         posts = [];
         neo_res.records.map((record) => posts.push(record._fields[0]));
         const user = await User.findById(req.id).select('newNotifications numberOfNewNotifications');
-        return res.status(200).send({ feed: posts, newNotifications: user.newNotifications, numberOfNewNotifications: user.numberOfNewNotifications });
+        return res.status(200).send({ feed: posts, newNotifications: user.newNotifications, numberOfNewNotifications: user.numberOfNewNotifications, skip: posts.length, end: posts.length === 0 });
     } catch (err) {
         console.log(err);
         return res.status(500).send("Server Error");
