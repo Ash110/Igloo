c1c947b2a1697eda3222e5c34484a966bf6caafd
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2021-02-28 22:43:58 +0530

    Upload images to S3

---
+++ aws.js
@@ -0,0 +1,106 @@
+var AWS = require("aws-sdk");
+var uuid = require('uuid');
+const fs = require('fs');
+const User = require('./models/User');
+const Post = require('./models/Post');
+const s3 = new AWS.S3();
+const config = require('config')
+const mongoose = require('mongoose')
+
+const getUsers = async () => {
+    try {
+        const db = config.get('mongoURI');
+        mongoose.set('useCreateIndex', true)
+        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, });
+        console.log("DB connected");
+        const users = await User.find().select('_id profilePicture headerImage');
+        for (let i of users) {
+            if (i.profilePicture != 'user.png') {
+                const fileContent = fs.readFileSync(`./images/profilepictures/${i.profilePicture}`);
+
+                // Setting up S3 upload parameters
+                const params = {
+                    Bucket: "iglooprofilepictures",
+                    Key: i.profilePicture, // File name you want to save as in S3
+                    Body: fileContent,
+                    ACL: 'public-read'
+                };
+
+                // Uploading files to the bucket
+                s3.upload(params, async (err, data) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(`File uploaded successfully. ${data.Location}`);
+                    await User.findByIdAndUpdate(i._id, { profilePicture: data.Location });
+                });
+            } else {
+                await User.findByIdAndUpdate(i._id, { profilePicture: "https://iglooprofilepictures.s3.ap-south-1.amazonaws.com/user.png" });
+            }
+            if (i.headerImage != 'default.png') {
+                const fileContent = fs.readFileSync(`./images/headerimages/${i.headerImage}`);
+
+                // Setting up S3 upload parameters
+                const params = {
+                    Bucket: "iglooheaderimages",
+                    Key: i.headerImage, // File name you want to save as in S3
+                    Body: fileContent,
+                    ACL: 'public-read'
+                };
+
+                // Uploading files to the bucket
+                s3.upload(params, async (err, data) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(`File uploaded successfully. ${data.Location}`);
+                    await User.findByIdAndUpdate(i._id, { headerImage: data.Location });
+                });
+            } else {
+                await User.findByIdAndUpdate(i._id, { headerImage: "https://iglooheaderimages.s3.ap-south-1.amazonaws.com/default.png" });
+            }
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+const getPosts = async () => {
+    try {
+        const db = config.get('mongoURI');
+        mongoose.set('useCreateIndex', true)
+        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, });
+        console.log("DB connected");
+        const posts = await Post.find().select('_id image isPagePost');
+        for (let i of posts) {
+            if (i.image) {
+                const fileContent = fs.readFileSync(`./images/posts/${i.image}`);
+
+                // Setting up S3 upload parameters
+                const params = {
+                    Bucket: "iglooposts",
+                    Key: i.image, // File name you want to save as in S3
+                    Body: fileContent,
+                    ACL: 'public-read'
+                };
+
+                // Uploading files to the bucket
+                s3.upload(params, async (err, data) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(`File uploaded successfully. ${data.Location}`);
+                    await Post.findByIdAndUpdate(i._id, { image: data.Location });
+                });
+            } 
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// getUsers();
+
+
+// getPosts();
--- routes/api/files.js
+++ routes/api/files.js
@@ -1,15 +1,20 @@
-const path = require('path');
 const fs = require('fs');
+var aws = require('aws-sdk')
+const path = require('path');
 const multer = require('multer');
+var multerS3 = require('multer-s3')
 const express = require('express');
+const exifremove = require('exifremove');
 const User = require('../../models/User');
-const Group = require('../../models/Group');
 const Post = require('../../models/Post');
+const Group = require('../../models/Group');
 const auth = require('../../middleware/auth');
 const neodriver = require('../../neo4jconnect');
-const exifremove = require('exifremove');
 
 const router = express.Router();
+
+var s3 = new aws.S3({})
+
 
 //@route   POST /api/files/uploadProfilePicture
 //@desc    Upload a new Profile Picture
@@ -17,29 +22,44 @@ const router = express.Router();
 
 router.post('/uploadProfilePicture', auth, (req, res) => {
     //Set storage engine
-    var imageName = '';
-    const storage = multer.diskStorage({
-        destination: './images/profilepictures/',
-        filename: (req, file, callback) => {
-            imageName = req.id + path.extname(file.originalname);
-            callback(null, req.id + path.extname(file.originalname));
-        }
-    })
+    // var imageName = '';
+    // const storage = multer.diskStorage({
+    //     destination: './images/profilepictures/',
+    //     filename: (req, file, callback) => {
+    //         imageName = req.id + path.extname(file.originalname);
+    //         callback(null, req.id + path.extname(file.originalname));
+    //     }
+    // })
 
     //Initialise Upload
-    const upload = multer({ storage }).single('profilepicture');
+    // const upload = multer({ storage }).single('profilepicture');
+    const upload = multer({
+        storage: multerS3({
+            s3: s3,
+            bucket: 'iglooprofilepictures',
+            acl: 'public-read',
+            metadata: function (req, file, cb) {
+                const imageName = req.id + path.extname(file.originalname);
+                cb(null, { fieldName: imageName });
+            },
+            key: function (req, file, cb) {
+                const imageName = req.id + path.extname(file.originalname);
+                cb(null, imageName);
+            }
+        })
+    }).single('profilepicture');
     //Start the upload
     upload(req, res, async (err) => {
         if (err) {
             console.log(err);
             return res.status(500).send("Server Error");
         } else {
-            console.log(req.file);
-            console.log(`Match (u:User {id : "${req.id}"}) SET u.profilePicture = "${imageName}"`);
-            await User.findOneAndUpdate({ _id: req.id }, { profilePicture: imageName });
+            const imageUrl = req.file.location;
+            console.log(`Match (u:User {id : "${req.id}"}) SET u.profilePicture = "${imageUrl}"`);
+            await User.findOneAndUpdate({ _id: req.id }, { profilePicture: imageUrl });
             const session = neodriver.session();
             try {
-                await session.run(`Match (u:User {id : "${req.id}"}) SET u.profilePicture = "${imageName}"`);
+                await session.run(`Match (u:User {id : "${req.id}"}) SET u.profilePicture = "${imageUrl}"`);
             } catch (e) {
                 console.log(e);
                 await session.close()
@@ -47,9 +67,7 @@ router.post('/uploadProfilePicture', auth, (req, res) => {
             } then = async () => {
                 await session.close()
             }
-            res.status(200).send(imageName);
-            const uploadedImage = fs.readFileSync(`./images/profilepictures/${imageName}`);
-            exifremove.remove(uploadedImage);
+            res.status(200).send(imageUrl);
         }
     });
 });
@@ -60,29 +78,45 @@ router.post('/uploadProfilePicture', auth, (req, res) => {
 
 router.post('/uploadHeaderImage', auth, (req, res) => {
     //Set storage engine
-    var imageName = '';
-    const storage = multer.diskStorage({
-        destination: './images/headerimages/',
-        filename: (req, file, callback) => {
-            imageName = req.id + path.extname(file.originalname);
-            callback(null, req.id + path.extname(file.originalname));
-        }
-    })
+    // var imageName = '';
+    // const storage = multer.diskStorage({
+    //     destination: './images/headerimages/',
+    //     filename: (req, file, callback) => {
+    //         imageName = req.id + path.extname(file.originalname);
+    //         callback(null, req.id + path.extname(file.originalname));
+    //     }
+    // })
 
-    //Initialise Upload
-    const upload = multer({ storage }).single('headerimage');
+    // //Initialise Upload
+    // const upload = multer({ storage }).single('headerimage');
+
+    const upload = multer({
+        storage: multerS3({
+            s3: s3,
+            bucket: 'iglooheaderimages',
+            acl: 'public-read',
+            metadata: function (req, file, cb) {
+                const imageName = req.id + path.extname(file.originalname);
+                cb(null, { fieldName: imageName });
+            },
+            key: function (req, file, cb) {
+                const imageName = req.id + path.extname(file.originalname);
+                cb(null, imageName);
+            }
+        })
+    }).single('headerimage');
+
     //Start the upload
     upload(req, res, async (err) => {
         if (err) {
             console.log(err);
             return res.status(500).send("Server Error");
         } else {
-            console.log(req.file);
-            console.log(`Match (u:User {id : "${req.id}"}) SET u.headerImage = "${imageName}"`);
-            await User.findOneAndUpdate({ _id: req.id }, { headerImage: imageName });
+            const imageUrl = req.file.location;
+            await User.findOneAndUpdate({ _id: req.id }, { headerImage: imageUrl });
             const session = neodriver.session();
             try {
-                await session.run(`Match (u:User {id : "${req.id}"}) SET u.headerImage = "${imageName}"`);
+                await session.run(`Match (u:User {id : "${req.id}"}) SET u.headerImage = "${imageUrl}"`);
             } catch (e) {
                 console.log(e);
                 await session.close()
@@ -90,9 +124,7 @@ router.post('/uploadHeaderImage', auth, (req, res) => {
             } then = async () => {
                 await session.close()
             }
-            res.status(200).send(imageName);
-            const uploadedImage = fs.readFileSync(`./images/headerimages/${imageName}`);
-            exifremove.remove(uploadedImage);
+            res.status(200).send(imageUrl);
         }
     });
 });
@@ -103,17 +135,33 @@ router.post('/uploadHeaderImage', auth, (req, res) => {
 
 router.post('/uploadImagePost', auth, (req, res) => {
     // Set storage engine
-    var imageName = '';
-    const storage = multer.diskStorage({
-        destination: './images/posts/',
-        filename: (req, file, callback) => {
-            imageName = req.id + "_" + Date.now() + path.extname(file.originalname);
-            callback(null, imageName);
-        }
-    })
+    // var imageName = '';
+    // const storage = multer.diskStorage({
+    //     destination: './images/posts/',
+    //     filename: (req, file, callback) => {
+    //         imageName = req.id + "_" + Date.now() + path.extname(file.originalname);
+    //         callback(null, imageName);
+    //     }
+    // })
 
-    //Initialise Upload
-    const upload = multer({ storage }).single('post');
+    // //Initialise Upload
+    // const upload = multer({ storage }).single('post');
+
+    const upload = multer({
+        storage: multerS3({
+            s3: s3,
+            bucket: 'iglooposts',
+            acl: 'public-read',
+            metadata: function (req, file, cb) {
+                const imageName = req.id + path.extname(file.originalname);
+                cb(null, { fieldName: imageName });
+            },
+            key: function (req, file, cb) {
+                const imageName = req.id + path.extname(file.originalname);
+                cb(null, imageName);
+            }
+        })
+    }).single('post');
 
     //Start the upload
     upload(req, res, async (err) => {
@@ -122,16 +170,14 @@ router.post('/uploadImagePost', auth, (req, res) => {
             return res.status(500).send("Server Error");
         } else {
             try {
-                console.log(req.file);
+                const imageUrl = req.file.location;
                 const post = new Post({
                     creator: req.id,
                     isText: false,
-                    image: imageName,
+                    image: imageUrl,
                 });
                 await post.save();
-                res.status(200).send({ imageName, postId: post._id });
-                const uploadedImage = fs.readFileSync(`./images/posts/${imageName}`);
-                exifremove.remove(uploadedImage);
+                res.status(200).send({ imageName: imageUrl, postId: post._id });
             } catch (err) {
                 console.log(err);
             }
@@ -145,17 +191,33 @@ router.post('/uploadImagePost', auth, (req, res) => {
 
 router.post('/uploadPageImagePost', auth, (req, res) => {
     // Set storage engine
-    var imageName = '';
-    const storage = multer.diskStorage({
-        destination: './images/pageposts/',
-        filename: (req, file, callback) => {
-            imageName = req.id + "_" + Date.now() + path.extname(file.originalname);
-            callback(null, imageName);
-        }
-    })
+    // var imageName = '';
+    // const storage = multer.diskStorage({
+    //     destination: './images/pageposts/',
+    //     filename: (req, file, callback) => {
+    //         imageName = req.id + "_" + Date.now() + path.extname(file.originalname);
+    //         callback(null, imageName);
+    //     }
+    // })
 
-    //Initialise Upload
-    const upload = multer({ storage }).single('post');
+    // //Initialise Upload
+    // const upload = multer({ storage }).single('post');
+
+    const upload = multer({
+        storage: multerS3({
+            s3: s3,
+            bucket: 'iglooposts',
+            acl: 'public-read',
+            metadata: function (req, file, cb) {
+                const imageName = req.id + path.extname(file.originalname);
+                cb(null, { fieldName: imageName });
+            },
+            key: function (req, file, cb) {
+                const imageName = req.id + path.extname(file.originalname);
+                cb(null, imageName);
+            }
+        })
+    }).single('post');
 
     //Start the upload
     upload(req, res, async (err) => {
@@ -163,17 +225,15 @@ router.post('/uploadPageImagePost', auth, (req, res) => {
             console.log(err);
             return res.status(500).send("Server Error");
         } else {
-            console.log(req.file);
+            const imageUrl = req.file.location;
             const post = new Post({
                 creator: req.id,
                 isText: false,
-                image: imageName,
+                image: imageUrl,
                 isPagePost: true,
             });
             await post.save();
-            res.status(200).send({ imageName, postId: post._id });
-            const uploadedImage = fs.readFileSync(`./images/pageposts/${imageName}`);
-            exifremove.remove(uploadedImage);
+            res.status(200).send({ imageName: imageUrl, postId: post._id });
         }
     });
 });
