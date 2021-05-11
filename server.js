9676771e663a0ed8576a4a3780a4d395d5b07085
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2021-05-11 16:35:45 +0530

    Go back to old CORS implementation

--- server.js
+++ server.js
@@ -24,20 +24,20 @@ app.use(mongoSanitize());
 app.use(xss());
 
 //Setting up CORS
-// app.use(cors());
-const allowedOrigins = ["http://localhost:3000",];
-app.use(function (req, res, next) {
-    let origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
-    }
+app.use(cors());
+// const allowedOrigins = ["http://localhost:3000",];
+// app.use(function (req, res, next) {
+//     let origin = req.headers.origin;
+//     if (allowedOrigins.includes(origin)) {
+//         res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
+//     }
 
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// });
 
 //Morgan for logging
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'reqs.log'), { flags: 'a' })
